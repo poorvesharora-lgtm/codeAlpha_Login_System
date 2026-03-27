63ff158a54d41977e9adc4fbbda12b301afab5db
poorvesharora-lgtm <poorvesharora@gmail.com>
2026-03-27 11:07:22 +0530

    Add files via upload

---
+++ CodeAlpha__loginsystem/login.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+void registerUser() {
+    string username, password;
+
+    cout << "\n--- Registration ---\n";
+    cout << "Enter username: ";
+    cin >> username;
+
+    cout << "Enter password: ";
+    cin >> password;
+
+    ofstream file("users.txt", ios::app);
+    file << username << " " << password << endl;
+    file.close();
+
+    cout << "Registration successful!\n";
+}
+
+void loginUser() {
+    string username, password;
+    string fileUser, filePass;
+    bool found = false;
+
+    cout << "\n--- Login ---\n";
+    cout << "Enter username: ";
+    cin >> username;
+
+    cout << "Enter password: ";
+    cin >> password;
+
+    ifstream file("users.txt");
+
+    while (file >> fileUser >> filePass) {
+        if (fileUser == username && filePass == password) {
+            found = true;
+            break;
+        }
+    }
+
+    file.close();
+
+   if(found){
+    cout << "Login sucessful! welcome " << username << endl;
+   }
+   else{
+    cout << "Invalid username or password\n";
+
+   }
+}
+
+int main() {
+    int choice;
+
+  do{
+    cout << "\n======== Login system=========\n";
+    cout << "1.Register\n";
+    cout << "2.Login\n";
+    cout << "3.Exit\n";
+    cout << "Enter your choice = ";
+    cin >> choice;
+
+    switch(choice){
+        case 1:
+        registerUser();
+        break;
+
+        case 2:
+        loginUser();
+        break;
+
+        case 3:
+        cout << "Exiting the Program....";
+        break;
+
+        default:
+        cout << "Invalid choice\n";
+
+
+    }
+
+   
+
+  }
+   while(choice!= 3);
+
+    return 0;
+}
